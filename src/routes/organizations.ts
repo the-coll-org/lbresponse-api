edcf3c5498fbf0fd8c95f4691b5d6d609b6f59b1
Elias Maalouf <elias.elghandourmaalouf@lau.edu>
2026-05-13 22:28:53 +0300

    feat(organizations): add map and get endpoints, refactor controller functions
    Extract buildMergedDtos and filterDtos functions from
    
    listOrganizations for modularity, add new MapProviderDto
    
    interface, and introduce /map and /:id routes for enhanced
    
    organization access.

--- src/routes/organizations.ts
+++ src/routes/organizations.ts
@@ -1,6 +1,12 @@
 import { Router } from 'express';
-import { listOrganizations } from '../controllers/organizationsController';
+import {
+  listOrganizations,
+  mapListOrganizations,
+  getOrganization,
+} from '../controllers/organizationsController';
 
 const router = Router();
+router.get('/map', mapListOrganizations);
+router.get('/:id', getOrganization);
 router.get('/', listOrganizations);
 export default router;
